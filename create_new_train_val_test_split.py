bdbb6cdf4a4e937c6933dd7e6e46a9c016502b34
Andq <andq@blockpass.org>
2020-07-21 13:11:44 +0700

    fix np is not defined

--- create_new_train_val_test_split.py
+++ create_new_train_val_test_split.py
@@ -1,5 +1,7 @@
-#%%
+# %%
 import pickle
+import numpy as np
+
 
 def create_new_train_val_test_split(save=False, holdout_percentage=0.15, train_percentage=0.7):
     """
@@ -8,25 +10,30 @@ def create_new_train_val_test_split(save=False, holdout_percentage=0.15, train_p
     Optionally, overwrites the original train_val_test_dict.pkl file, which will be done if run directly as a script.
     """
 
-    tumor_type_dict = pickle.load(open( "tumor_type_dict.pkl", "rb" ) )
+    tumor_type_dict = pickle.load(open("tumor_type_dict.pkl", "rb"))
     completelist = list(tumor_type_dict.keys())
 
-    np.random.shuffle(completelist) # shuffles in place
+    np.random.shuffle(completelist)  # shuffles in place
 
-    train_val_test_dict={}
+    train_val_test_dict = {}
 
-    holdout_percentage=holdout_percentage
-    train_val_test_dict['test']=completelist[0:int(len(completelist)*holdout_percentage)]
-    trainlist=completelist[int(len(completelist)*holdout_percentage):len(completelist)]
+    holdout_percentage = holdout_percentage
+    train_val_test_dict['test'] = completelist[0:int(
+        len(completelist)*holdout_percentage)]
+    trainlist = completelist[int(
+        len(completelist)*holdout_percentage):len(completelist)]
 
-    train_percentage=train_percentage
-    train_val_test_dict['train']=trainlist[0:int(len(trainlist)*train_percentage)]
-    train_val_test_dict['val']=trainlist[int(len(trainlist)*train_percentage):len(trainlist)]
+    train_percentage = train_percentage
+    train_val_test_dict['train'] = trainlist[0:int(
+        len(trainlist)*train_percentage)]
+    train_val_test_dict['val'] = trainlist[int(
+        len(trainlist)*train_percentage):len(trainlist)]
 
-    if save==True:
-        pickle.dump( train_val_test_dict, open( "train_val_test_dict.pkl", "wb" ) )
+    if save == True:
+        pickle.dump(train_val_test_dict, open("train_val_test_dict.pkl", "wb"))
 
     return train_val_test_dict
+
 
 if __name__ == "__main__":
     create_new_train_val_test_split(save=False)
